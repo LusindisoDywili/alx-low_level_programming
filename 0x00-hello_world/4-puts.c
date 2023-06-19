03a9c306bf8c381b1dbddd3b39b994ad62422e13
LusindisoDywili <lusindisosfisodywili@gmail.com>
2023-06-19 15:01:22 -0700

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - print out a line not using printf but puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
