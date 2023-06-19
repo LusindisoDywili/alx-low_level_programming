636f406b8cb97eb8068c55ffa8e36f4cf85b71b5
LusindisoDywili <lusindisosfisodywili@gmail.com>
2023-06-19 15:12:05 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - use printf function to prit sets of words
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+	return (0);
+}
